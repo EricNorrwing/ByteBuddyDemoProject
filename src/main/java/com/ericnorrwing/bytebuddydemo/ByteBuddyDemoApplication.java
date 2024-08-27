e03e5ffc7d8cceecc085ba1b5f24277f65d558cb
EricNorrwing <eric.norrwing@gmail.com>
2024-08-27 16:29:23 +0200

    First and only commit, code works as expected.

---
+++ src/main/java/com/ericnorrwing/bytebuddydemo/ByteBuddyDemoApplication.java
@@ -0,0 +1,13 @@
+package com.ericnorrwing.bytebuddydemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ByteBuddyDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ByteBuddyDemoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/ericnorrwing/bytebuddydemo/ByteBuddyDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ericnorrwing.bytebuddydemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ByteBuddyDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
